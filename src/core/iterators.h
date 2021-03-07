532d5db65b2494568f3f10a8ebc9619e0d4a2cc5
schnedann <schnedann@users.noreply.github.com>
2021-03-07 22:41:35 +0100

    updated copyright dates

--- src/core/iterators.h
+++ src/core/iterators.h
@@ -5,7 +5,7 @@
 /******************************************************************************/
 
 /*
- * Copyright (c) 2020, Danny Schneider
+ * Copyright (c) 2021, Danny Schneider
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
